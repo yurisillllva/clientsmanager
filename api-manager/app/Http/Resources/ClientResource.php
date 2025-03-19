1094ac80aa60647b648c90816fcdfde4ade53720
Yuri Silva <ysm281@gmail.com>
2025-03-19 15:50:56 -0300

    create client resource to formatting data

---
+++ api-manager/app/Http/Resources/ClientResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'city' => $this->city,
+            'state' => $this->state,
+            'photo' => $this->photo,
+            'age' => $this->age
+        ];
+    }
+}
