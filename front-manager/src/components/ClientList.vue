9a60a94604e2e4908c779deaccfa0a85f5e2c790
Yuri Silva <ysm281@gmail.com>
2025-03-19 13:57:16 -0300

    list client adjustments

--- front-manager/src/components/ClientList.vue
+++ front-manager/src/components/ClientList.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <input v-model="search" @input="fetchClients" placeholder="Buscar...">
+    <div>
+    <button @click="showCreateModal">Novo Cliente</button>
+    
+    <ClientModal
+      :show="showModal"
+      :client="selectedClient"
+      @submit="handleSubmit"
+      @close="closeModal"
+    />
+
+    <DeleteConfirmation
+      :show="showDeleteModal"
+      @confirm="confirmDelete"
+      @cancel="closeDeleteModal"
+    />
+  </div>
     <table>
       <tr v-for="client in clients" :key="client.id">
         <td>{{ client.name }}</td>
@@ -23,29 +39,84 @@
 export default {
   data() {
     return {
+      showModal: false,
+      showDeleteModal: false,
+      selectedClient: null,
+      clientToDelete: null,
       clients: [],
       currentPage: 1,
       search: ''
     }
   },
   methods: {
-    async fetchClients() {
-      const { data } = await axios.get('clients', {
-        params: { page: this.currentPage, search: this.search }
-      });
-      this.clients = data.data;
+    showCreateModal() {
+      this.selectedClient = null;
+      this.showModal = true;
     },
-    makeCall(client) {
-      axios.post(`clients/${client.id}/call`);
-    }
-  },
-  watch: {
-    currentPage() {
+
+    editClient(client) {
+      this.selectedClient = client;
+      this.showModal = true;
+    },
+
+    deleteClient(client) {
+      this.clientToDelete = client;
+      this.showDeleteModal = true;
+    },
+
+    async handleSubmit(formData) {
+      try {
+        if (this.isEditing) {
+          await axios.put(`clients/${this.selectedClient.id}`, formData);
+        } else {
+          await axios.post('clients', formData);
+        }
+        this.fetchClients();
+        this.closeModal();
+      } catch (error) {
+        alert('Erro ao salvar!');
+      }
+    },
+
+    async confirmDelete() {
+      await axios.delete(`clients/${this.clientToDelete.id}`);
+      this.fetchClients();
+      this.closeDeleteModal();
+    },
+
+    async fetchClients() {
+      try {
+        const { data } = await axios.get('clients', {
+          params: { 
+            page: this.currentPage, 
+            search: this.search 
+          }
+        });
+        this.clients = data.data;
+        this.totalPages = data.last_page;
+      } catch (error) {
+        console.error('Erro ao buscar clientes:', error);
+      }
+    },
+
+    async makeCall(client) {
+      try {
+        await axios.post(`clients/${client.id}/call`);
+        alert('Chamada iniciada!');
+      } catch (error) {
+        alert('Erro ao iniciar chamada');
+      }
+    },
+
+    watch: {
+      currentPage() {
+        this.fetchClients();
+      }
+    },
+
+    mounted() {
       this.fetchClients();
     }
-  },
-  mounted() {
-    this.fetchClients();
   }
 }
 </script>
