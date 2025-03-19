13d8b50e620f5864b5597c30a5c493f9baa62a9b
Yuri Silva <ysm281@gmail.com>
2025-03-19 18:20:39 -0300

    adjustments in client controller

--- api-manager/app/Http/Controllers/Api/ClientController.php
+++ api-manager/app/Http/Controllers/Api/ClientController.php
@@ -2,19 +2,51 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Repositories\ClientRepository;
 use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+use App\Http\Resources\ClientResource;
 
 class ClientController extends Controller
 {
     public function index(ClientRepository $repository)
     {
-        return $repository->paginate(auth()->id());
+        try {
+            $clients = $repository->paginate(auth()->id());
+            return ClientResource::collection($clients);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro interno'], 500);
+        }
     }
 
     public function store(StoreClientRequest $request, ClientRepository $repository)
     {
-        $client = $repository->create(auth()->id(), $request->validated());
-        return response()->json($client, 201);
+        try {
+            $client = $repository->create(auth()->id(), $request->validated());
+            return (new ClientResource($client))
+                ->response()
+                ->setStatusCode(201);
+        } catch (\Illuminate\Database\QueryException $e) {
+            return response()->json(['error' => 'Erro de banco de dados'], 500);
+        }
+    }
+    
+    public function show(ClientRepository $repository, $id)
+    {
+        $client = $repository->findOrFail(auth()->id(), $id);
+        return new ClientResource($client);
+    }
+
+    public function update(UpdateClientRequest $request, ClientRepository $repository, $id)
+    {
+        $client = $repository->update(auth()->id(), $id, $request->validated());
+        return new ClientResource($client);
+    }
+
+    public function destroy(ClientRepository $repository, $id)
+    {
+        $repository->delete(auth()->id(), $id);
+        return response()->json(null, 204);
     }
 }
