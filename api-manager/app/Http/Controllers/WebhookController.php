036da2f51a4a770f8874c265db31f1c351447c48
Yuri Silva <ysm281@gmail.com>
2025-03-18 23:46:04 -0300

    create webhookcontroller

---
+++ api-manager/app/Http/Controllers/WebhookController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebhookController extends Controller
+{
+    public function handleClient(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'sometimes',
+            'city' => 'sometimes',
+            'state' => 'sometimes|size:2',
+            'photo' => 'sometimes|url',
+            'age' => 'sometimes|integer',
+        ]);
+
+        $client = Client::create($validated);
+        return response()->json($client);
+    }
+}
