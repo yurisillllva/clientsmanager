53de360ddb314df1489b9552a0dfb8a73a61800d
Yuri Silva <ysm281@gmail.com>
2025-03-19 09:18:24 -0300

    create login vue

---
+++ front-manager/src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="login">
+    <input v-model="form.email" type="email" required>
+    <input v-model="form.password" type="password" required>
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: { email: '', password: '' }
+    }
+  },
+  methods: {
+    async login() {
+      const { data } = await axios.post('login', this.form);
+      localStorage.setItem('token', data.token);
+      this.$router.push('/dashboard');
+    }
+  }
+}
+</script>
