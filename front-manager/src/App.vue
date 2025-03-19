b3f423b582fa36c976067d2ced0aebb3882d887f
Yuri Silva <ysm281@gmail.com>
2025-03-19 20:44:28 -0300

    adjustments for use view router

--- front-manager/src/App.vue
+++ front-manager/src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  </template>
+  <router-view />
+</template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Estilos globais (opcional) */
+body {
+  margin: 0;
+  font-family: 'Arial', sans-serif;
 }
 </style>
