eebed854a033d02dc115b52a27940856ee734479
Yuri Silva <ysm281@gmail.com>
2025-03-18 23:13:09 -0300

    migration clients

---
+++ api-manager/database/migrations/2025_03_19_020640_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->index();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable()->index();
+            $table->string('city')->nullable();
+            $table->char('state', 2)->nullable();
+            $table->string('photo')->nullable();
+            $table->integer('age')->nullable();
+            $table->boolean('welcome_email_sent')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
