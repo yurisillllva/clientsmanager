70eb44cfa639995ad7a0ae5720579631fd0bb570
Yuri Silva <ysm281@gmail.com>
2025-03-19 12:08:49 -0300

    seed login user test

---
+++ api-manager/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@huggy.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
