9399c7c87997270d89319b97a41718f179c768a7
Yuri Silva <ysm281@gmail.com>
2025-03-19 20:41:47 -0300

    creat file for modularize and standardize routes

---
+++ front-manager/src/router/index.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Welcome from '@/views/WelcomeView.vue';
+import Login from '@/views/LoginView.vue';
+import Dashboard from '@/views/DashboardView.vue';
+
+const routes = [
+  { 
+    path: '/',
+    component: Welcome,
+    meta: { title: 'Bem-vindo' }
+  },
+  { 
+    path: '/login', 
+    component: Login,
+    meta: { title: 'Login' }
+  },
+  { 
+    path: '/dashboard', 
+    component: Dashboard,
+    meta: { 
+      requiresAuth: true,
+      title: 'Dashboard'
+    }
+  }
+];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
+
+
+router.beforeEach((to, from, next) => {
+
+  document.title = to.meta.title || 'Huggy Challenge';
+
+  // Verifica o token de autenticação
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      next('/login');
+      return;
+    }
+  }
+  
+  next();
+});
+
+export default router;
