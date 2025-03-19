b886b966350f72dc627c01a7f3bfbf69b7d3bf58
Yuri Silva <ysm281@gmail.com>
2025-03-19 15:43:36 -0300

    fixes in webhookController

--- api-manager/app/Http/Controllers/WebhookController.php
+++ api-manager/app/Http/Controllers/WebhookController.php
@@ -10,16 +10,16 @@ class WebhookController extends Controller
     public function handleClient(Request $request)
     {
         $validated = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'sometimes',
-            'city' => 'sometimes',
-            'state' => 'sometimes|size:2',
-            'photo' => 'sometimes|url',
-            'age' => 'sometimes|integer',
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:clients',
+            'phone' => 'required|string|max:20',
+            'city' => 'nullable|string',
+            'state' => 'nullable|string|size:2',
+            'photo' => 'nullable|url',
+            'age' => 'nullable|integer|min:1'
         ]);
 
         $client = Client::create($validated);
-        return response()->json($client);
+        return response()->json($client, 201);
     }
 }
