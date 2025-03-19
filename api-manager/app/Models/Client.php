79570a8736bd8e7e55ff10ab9efaa7f0bf391a57
Yuri Silva <ysm281@gmail.com>
2025-03-18 23:20:16 -0300

    create model client

---
+++ api-manager/app/Models/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = [
+        'user_id', 'name', 'email', 'phone', 
+        'city', 'state', 'photo', 'age'
+    ];
+}
