9ac18212ad9e0ce3eb9f7bdccbc1db0fc1bd52a4
Yuri Silva <ysm281@gmail.com>
2025-03-19 15:45:03 -0300

    rule to update mabe missing some fields

---
+++ api-manager/app/Http/Requests/UpdateClientRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('clients')->ignore($this->client)
+            ]
+        ]);
+    }
+}
