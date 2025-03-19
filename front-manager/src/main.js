31f27271b0c9c8b785575f89544ea9459bb07c7c
Yuri Silva <ysm281@gmail.com>
2025-03-19 00:07:54 -0300

    config axios in front with api

--- front-manager/src/main.js
+++ front-manager/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
 
 createApp(App).mount('#app')
