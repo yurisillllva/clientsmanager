59933054774fbdc10b5ca72000023ac2e6b43cbf
Yuri Silva <ysm281@gmail.com>
2025-03-19 09:20:26 -0300

    possible screen to graphics

---
+++ front-manager/src/components/ChartsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="charts-container">
+    <canvas id="statesChart"></canvas>
+    <canvas id="citiesChart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  async mounted() {
+    const { data: states } = await axios.get('charts/states');
+    const { data: cities } = await axios.get('charts/cities');
+    
+    new Chart(document.getElementById('statesChart'), {
+      type: 'pie',
+      data: {
+        labels: states.labels,
+        datasets: [{
+          data: states.data,
+          backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe']
+        }]
+      }
+    });
+
+    // Repetir para cities
+  }
+}
+</script>
