8be0882f224231b3ee17f958fe9367dff14e061b
Yuri Silva <ysm281@gmail.com>
2025-03-19 10:22:02 -0300

    validate in api

---
+++ api-manager/app/Http/Requests/StoreClientRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:clients',
+            'phone' => 'required|string|max:20',
+            'age' => 'nullable|integer|min:1',
+            'photo' => 'nullable|url'
+        ];
+    }
+}
