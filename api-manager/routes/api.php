4018cd25ebb2eb35ab9831f07c2c073f389f26fd
Yuri Silva <ysm281@gmail.com>
2025-03-19 18:20:22 -0300

    adjustments in api

--- api-manager/routes/api.php
+++ api-manager/routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ChartController;
+use App\Http\Controllers\Api\VoipController;
+use App\Http\Controllers\WebhookController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +25,14 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('clients', ClientController::class);
     
-    Route::post('clients/{client}/call', [VoipController::class, 'initiateCall']);
     Route::get('charts/states', [ChartController::class, 'states']);
     Route::get('charts/cities', [ChartController::class, 'cities']);
+    Route::post('clients/{client}/call', [VoipController::class, 'initiateCall']);
 });
 
 // Webhook
+// Route::post('webhook/huggy-flow', [WebhookController::class, 'handleHuggyFlow'])
+//     ->withoutMiddleware(['auth:api', 'throttle']);
+
 Route::post('webhook/client', [WebhookController::class, 'handleClient'])
     ->middleware('verify.webhook');
