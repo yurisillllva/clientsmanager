e4d0d2e9a4aed50e2ca10e8898b345fd693c8699
Yuri Silva <ysm281@gmail.com>
2025-03-19 20:45:52 -0300

    create dashboard view

---
+++ front-manager/src/views/DashboardView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard">
+    <div class="d-flex justify-content-between align-items-center mb-2">
+    <header>
+      <nav>
+        <button @click="logout">🚪 Sair</button>
+      </nav>
+    </header>
+    </div>
+    
+    <ClientList />
+    <router-view /> <!-- Ver os gŕaficos -->
+  </div>
+</template>
+
+<script>
+import ClientList from '@/components/ClientList.vue'
+
+export default {
+  components: { ClientList },
+  methods: {
+    showCharts() {
+      this.$router.push('/dashboard/charts')
+    },
+    logout() {
+      localStorage.removeItem('token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
