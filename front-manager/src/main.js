f75456f174922c1054492634a4a59b466c53aeb5
Yuri Silva <ysm281@gmail.com>
2025-03-19 09:21:33 -0300

    insertion bootstrap in project

--- front-manager/src/main.js
+++ front-manager/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.interceptors.request.use(config => {
