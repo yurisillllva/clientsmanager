73f348dbde52d0a7cdd639e7c8256c9bb5c1943a
Yuri Silva <ysm281@gmail.com>
2025-03-20 17:09:30 -0300

    in axion send token

--- front-manager/src/main.js
+++ front-manager/src/main.js
@@ -10,6 +10,13 @@ axios.defaults.baseURL = 'http://localhost:8000/api';
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
+axios.interceptors.request.use(config => {
+   const token = localStorage.getItem('token');
+   if (token) {
+       config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
+});
 
 app.use(router)
    .mount('#app');
