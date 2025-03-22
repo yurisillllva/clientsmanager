5a40081abe1de6625e4d10fa2f1c3bb569d8479e
Yuri Silva <ysm281@gmail.com>
2025-03-22 17:53:29 -0300

    fix in voip controller for get token

--- api-manager/app/Http/Controllers/Api/VoipController.php
+++ api-manager/app/Http/Controllers/Api/VoipController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Client;
 use Twilio\Rest\Client as TwilioClient;
 use Illuminate\Http\Request;
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\VoiceGrant;
 
 class VoipController extends Controller
 {
@@ -16,34 +18,32 @@ class VoipController extends Controller
                 'error' => 'O cliente não possui um número de telefone cadastrado.'
             ], 400);
         }
-    
+
         // Formata o número de telefone (adiciona o código do país, se necessário)
         $phone = $this->formatPhoneNumber($client->phone);
-    
-        $twilio = new TwilioClient(
-            config('services.twilio.sid'),
-            config('services.twilio.auth_token')
+
+        $token = new AccessToken(
+            config('services.twilio.account_sid'),
+            config('services.twilio.api_key'),
+            config('services.twilio.api_secret'),
+            3600,
+            'web_client_' . uniqid()
         );
-    
-        try {
-            $call = $twilio->calls->create(
-                $phone, // Número de telefone formatado
-                config('services.twilio.from'), // Número Twilio
-                ["url" => "http://demo.twilio.com/docs/voice.xml"] // URL do TwiML
-            );
-    
-            return response()->json([
-                'message' => 'Chamada VOIP iniciada!',
-                'sid' => $call->sid
-            ], 200);
-    
-        } catch (\Exception $e) {
-            return response()->json([
-                'error' => 'Falha na ligação: ' . $e->getMessage()
-            ], 500);
-        }
+
+        $voiceGrant = new VoiceGrant();
+        $voiceGrant->setOutgoingApplicationSid(config('services.twilio.twiml_app_sid'));
+
+        
+        $token->addGrant($voiceGrant);
+
+        return response()->json([
+            'token' => $token->toJwt(),
+            'to' => $phone
+        ]);
+
+        
     }
-    
+
     /**
      * Formata o número de telefone para incluir o código do país (+55).
      */
@@ -51,12 +51,12 @@ class VoipController extends Controller
     {
         // Remove todos os caracteres não numéricos
         $phone = preg_replace('/[^0-9]/', '', $phone);
-    
+
         // Adiciona o código do país (+55) se o número não começar com +
         if (!str_starts_with($phone, '+')) {
             $phone = '+55' . $phone;
         }
-    
+
         return $phone;
     }
 }
