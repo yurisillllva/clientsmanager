703dbc2e8550d7afd3e729de223bb0bbceefec8a
Yuri Silva <ysm281@gmail.com>
2025-03-22 19:13:30 -0300

    layout email welcome

--- api-manager/resources/views/emails/welcome.blade.php
+++ api-manager/resources/views/emails/welcome.blade.php
@@ -4,9 +4,9 @@
     <title>Bem-vindo Meu novo clinte</title>
 </head>
 <body>
-    <h1>Olá, {{ $client->name }}!</h1>
-    <p>Seja bem-vindo. Estamos felizes em tê-lo conosco.</p>
-    <p>Seu email de cadastro é: {{ $client->email }}</p>
+    <h1>Olá, {{ $name }}!</h1>
+    <p>Seja bem-vindo. Agora você está concorrendo a participar da promoção.</p>
+    <p>Seu email de cadastro é: {{ $email }}</p>
     <p>Atenciosamente,<br>Equipe Yuri Silva</p>
 </body>
 </html>
