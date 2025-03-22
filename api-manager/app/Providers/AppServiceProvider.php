b110cbb383608d278391972d0a127222c4f3e15d
Yuri Silva <ysm281@gmail.com>
2025-03-22 05:51:20 -0300

    add chart in serviceprovider

--- api-manager/app/Providers/AppServiceProvider.php
+++ api-manager/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\ChartRepository; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            \App\Repositories\ChartRepository::class,
+            function ($app) {
+                return new ChartRepository();
+            }
+        );
     }
 
     /**
