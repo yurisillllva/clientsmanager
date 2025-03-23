2308621811d48e47cc9bf1d1cc43e93b31bc7a03
Yuri Silva <ysm281@gmail.com>
2025-03-23 04:53:58 -0300

    add hasfactory

--- api-manager/app/Models/Client.php
+++ api-manager/app/Models/Client.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'user_id', 'name', 'email', 'phone', 
         'city', 'state', 'photo', 'age', 'welcome_email_sent'
