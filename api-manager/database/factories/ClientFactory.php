de9b3545ac0d277e7d16c0a534f3bc20fe05a095
Yuri Silva <ysm281@gmail.com>
2025-03-23 04:54:56 -0300

    create factory client

---
+++ api-manager/database/factories/ClientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = Client::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'user_id' => \App\Models\User::factory(),
+        ];
+    }
+}
